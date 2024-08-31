927c8e80546f9b6410ffe0dad2ab8c0b34df512a
Benno <107504783+BennoCrafter@users.noreply.github.com>
2024-08-31 18:46:23 +0000

    code review. fixed bugs.

--- main.py
+++ main.py
@@ -11,6 +11,22 @@ from src.utils.load_config import load_config
 from src.utils.get_quantity_number import get_quantity_number
 from src.utils.load_file import load_file
 
+load_dotenv()
+config = load_config("assets/config.yaml")
+session_file_path = Path(config["session_file"])
+# Delete session.txt file if required by configuration.
+if session_file_path.exists() and config.get("delete_session_file_on_start"):
+    session_file_path.unlink()
+
+creds = botlib.Creds(homeserver=os.getenv('HOMESERVER'),
+                     username=os.getenv('USERNAME'),
+                     password=os.getenv('PASSWORD'))
+bot = botlib.Bot(creds)
+
+help_message = load_file(config["help_message_file"])
+active_polls = []
+PREFIX = config["prefix"]
+
 
 def load_configuration():
     """Load environment variables and configuration file."""
@@ -21,22 +37,15 @@ def load_configuration():
 
 def initialize_bot(config):
     """Initialize bot credentials and return bot instance."""
-    creds = botlib.Creds(
-        homeserver=os.getenv('HOMESERVER'),
-        username=os.getenv('USERNAME'),
-        password=os.getenv('PASSWORD')
-    )
+    creds = botlib.Creds(homeserver=os.getenv('HOMESERVER'),
+                         username=os.getenv('USERNAME'),
+                         password=os.getenv('PASSWORD'))
     return botlib.Bot(creds)
 
 
-def clean_up_session_file(session_file_path: Path, should_delete: bool):
-    """Delete session.txt file if required by configuration."""
-    if session_file_path.exists() and should_delete:
-        session_file_path.unlink()
-
-
 async def handle_error(room, event) -> None:
-    await bot.api.send_reaction(room.room_id, event, config["reaction"]["error"])
+    await bot.api.send_reaction(room.room_id, event,
+                                config["reaction"]["error"])
 
 
 async def get_sender_name(sender: str) -> str:
@@ -54,7 +63,8 @@ def is_valid(match: botlib.MessageMatch, valid_commands: list[str]) -> bool:
 
 def get_active_poll_in_room(room_id: str) -> Poll | None:
     """Check if there is an active poll in the given room and return it."""
-    return next((poll for poll in active_polls if room_id == poll.room.room_id), None)
+    return next(
+        (poll for poll in active_polls if room_id == poll.room.room_id), None)
 
 
 async def handle_message(room, message, match, config):
@@ -82,16 +92,16 @@ async def create_poll(room, message, match):
     title = ' '.join(match.args())
     await bot.api.send_markdown_message(room.room_id, f"## {title}")
     active_polls.append(
-        Poll(id=len(active_polls), name=title, room=room, item_entries=[])
-    )
+        Poll(id=len(active_polls), name=title, room=room, item_entries=[]))
 
 
 async def close_poll(room):
     poll = get_active_poll_in_room(room.room_id)
     if poll:
         await bot.api.send_markdown_message(
-            room.room_id, poll.formated_markdown(f"## {config['status_title']} {poll.name} (closed):")
-        )
+            room.room_id,
+            poll.formated_markdown(
+                f"## {config['status_title']} {poll.name} (closed):"))
         active_polls.remove(poll)
 
 
@@ -99,7 +109,8 @@ async def list_items(room):
     poll = get_active_poll_in_room(room.room_id)
     if poll:
         await bot.api.send_markdown_message(
-            room.room_id, poll.formated_markdown(f"## {config['status_title']} {poll.name}:")
+            room.room_id,
+            poll.formated_markdown(f"## {config['status_title']} {poll.name}:")
         )
 
 
@@ -113,11 +124,13 @@ async def remove_item(room, message, match):
     quantity_num, item_name = get_quantity_number(body_msg)
 
     count = quantity_num or config["default_quantity_number"]
+    item_name = item_name or body_msg
 
     item = poll.get_item(item_name)
     msg_sender = await get_sender_name(message.sender)
 
-    if not item or msg_sender not in item.user_count or count > item.user_count[msg_sender]:
+    if not item or msg_sender not in item.user_count or count > item.user_count[
+            msg_sender]:
         await handle_error(room, message)
         return
 
@@ -125,7 +138,8 @@ async def remove_item(room, message, match):
     if not item.user_count:
         poll.remove_item(item)
 
-    await bot.api.send_reaction(room.room_id, message, config["reaction"]["removed"])
+    await bot.api.send_reaction(room.room_id, message,
+                                config["reaction"]["removed"])
 
 
 async def add_item_to_poll(room, message, match, config):
@@ -133,21 +147,21 @@ async def add_item_to_poll(room, message, match, config):
     if not poll:
         return
 
-    body_msg = (
-        message.body.strip() if not config["use_add_command"]
-        else ' '.join(match.args()).strip()
-    )
+    body_msg = (message.body.strip() if not config["use_add_command"] else
+                ' '.join(match.args()).strip())
 
     if not body_msg:
         return
 
     quantity_num, item_name = get_quantity_number(body_msg)
-    count = quantity_num or 1
+
+    count = quantity_num or config["default_quantity_number"]
     item_name = item_name or body_msg
     sender_name = await get_sender_name(message.sender)
 
     poll.add_response(item_name, sender_name, count)
-    await bot.api.send_reaction(room.room_id, message, config["reaction"]["success"])
+    await bot.api.send_reaction(room.room_id, message,
+                                config["reaction"]["success"])
 
 
 @bot.listener.on_message_event
@@ -163,23 +177,6 @@ async def on_reaction(room, reaction, k):
     pass  # Reaction handler
 
 
-async def main():
-    while True:
-        try:
-            await bot.start()
-        except KeyboardInterrupt:
-            exit(0)
-        except Exception:
-            time.sleep(60)
-            continue
-
-
 if __name__ == "__main__":
-    config = load_configuration()
-    session_file_path = Path(config["session_file"])
-    clean_up_session_file(session_file_path, config.get("delete_session_file_on_start"))
-    bot = initialize_bot(config)
-    help_message = load_file(config["help_message_file"])
-    active_polls = []
-
-    asyncio.run(main())
+    print("Starting Bot...")
+    bot.run()
