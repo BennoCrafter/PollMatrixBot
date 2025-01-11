a710a9e49fa036b1f8d572370501722bfa08a88b
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-01-11 14:12:36 +0100

    Fixed valid function

--- src/commands/command.py
+++ src/commands/command.py
@@ -6,7 +6,6 @@ from src.poll_manager import PollManager
 from nio.events.room_events import RoomMessageText
 from src.utils.logging_config import setup_logger
 
-
 class Command(ABC):
     def __init__(self, trigger_names: list[str]) -> None:
         self.trigger_names = trigger_names
@@ -17,10 +16,9 @@ class Command(ABC):
         self.poll_manager = PollManager()
         self.logger = setup_logger(__name__)
 
-
     def is_valid(self, match: botlib.MessageMatch) -> bool:
-        command = match.event.body[len(match._prefix):].split()[0]
-        return command in self.trigger_names
+        return match.command() in self.trigger_names
+
 
     def load(self) -> None:
         ...
