4fae1f148273aece55ff3decfa7737e026797681
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-03-29 19:00:37 +0100

    yummy

--- src/commands/add_item_command.py
+++ src/commands/add_item_command.py
@@ -1,4 +1,4 @@
-
+import random
 from src.commands.command import Command
 from src.command_structure import CommandStructure
 
@@ -11,4 +11,9 @@ class AddCommand(Command):
         super().__init__(trigger_names)
 
     async def execute(self, structure: CommandStructure, **kwargs) -> None:
+        await self.check_triggers(structure)
         await self.poll_manager.add_item(structure)
+
+    async def check_triggers(self, structure: CommandStructure):
+        if structure.args_string and "hawaii" in structure.args_string.lower():
+            await self.bot.api.send_text_message(structure.match.room.room_id, random.choice(["Yummy! 😋", "Pineapple perfection! 🍍", "Aloha! 🏝️"]))
