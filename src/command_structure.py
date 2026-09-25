a54abb45f53125e88dcf2362f073eb039fdd241f
Benno <107504783+BennoCrafter@users.noreply.github.com>
2026-09-25 22:32:32 +0200

    resolve #20
    Now are command inputs likes `!Lunchy` also detected as `!lunchy`command. Resolves issue #20 case sensitivity

--- src/command_structure.py
+++ src/command_structure.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Optional
+
 import simplematrixbotlib
+
 
 @dataclass
 class CommandStructure:
@@ -9,8 +11,9 @@ class CommandStructure:
     match: simplematrixbotlib.MessageMatch
 
     @classmethod
-    def from_string(cls, input_text: str, prefix: str, match: simplematrixbotlib.MessageMatch) -> Optional['CommandStructure']:
-
+    def from_string(
+        cls, input_text: str, prefix: str, match: simplematrixbotlib.MessageMatch
+    ) -> Optional["CommandStructure"]:
         if not input_text.startswith(prefix):
             return None
 
@@ -18,9 +21,9 @@ class CommandStructure:
         if not parts:
             return None
 
-        command = parts[0][len(prefix):] # remove prefix
+        command = parts[0][len(prefix) :].lower()  # remove prefix
 
-        args_string = input_text[len(prefix) + len(command):].strip()
+        args_string = input_text[len(prefix) + len(command) :].strip()
         if not args_string:
             args_string = None
 
